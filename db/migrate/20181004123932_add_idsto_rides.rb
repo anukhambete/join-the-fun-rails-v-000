f3d5ae27f4d3de245850752f4b0cda23f6ea6e6a
Anu Khambete <anukhambete@gmail.com>
2018-10-04 12:40:54 +0000

    automatically backed up by learn

--- db/migrate/20181004123932_add_idsto_rides.rb
+++ db/migrate/20181004123932_add_idsto_rides.rb
@@ -2,7 +2,7 @@ class AddIdstoRides < ActiveRecord::Migration
   def change
     change_table :rides do |t|
       t.integer :passenger_id
-      t.integer :passenger_id
+      t.integer :taxi_id
     end
   end
 end
